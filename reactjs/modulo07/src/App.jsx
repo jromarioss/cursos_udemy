bcb4d90db62979fa373b63c57080ee7bc325502c
JromarioSs <jromario2014@gmail.com>
2023-09-16 08:25:56 -0300

    reactjs modulo07 hooks

--- reactjs/modulo07/src/App.jsx
+++ reactjs/modulo07/src/App.jsx
@@ -1,32 +1,59 @@
 import { useEffect, useState } from 'react'
+
+import { useFetch } from "./hooks/useFetch"
+
 import './App.css'
 
+const url = "http://localhost:3333/products";
+
 function App() {
-  const [products, setProducts] = useState([]);
-  const url = "http://localhost:3333/products";
+  const { data, httpConfig, loading } = useFetch(url);
 
-  async function fetchData() {
-    const res = await fetch(url)
-    const data = await res.json();
-    setProducts(data)
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    const product = { name, price };
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
   }
-
-  useEffect(() => {
-    fetchData()
-  },[])
-
-  console.log(products)
 
   return (
     <div className='App'>
       <h1>lista de produtos</h1>
-      <ul>
-        {products.map((products) => {
-          return (
-            <li key={products.id}>{products.name} - R$: {products.price}</li>
-          )
-        })}
-      </ul>
+      <div className='addProduct'>
+        <form onSubmit={handleSubmit}>
+          <label> Name:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label> Preço $:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          {loading ?
+            <input type="submit" value="salvando" disabled />
+            :
+            <input type="submit" value="enviar" />
+          }
+        </form>
+      </div>
+      {loading ?
+        <p>Carregando</p>
+        :
+        <ul>
+          {data && data.map((products) => {
+            return (
+              <li key={products.id}>{products.name} - R$: {products.price}</li>
+            )
+          })}
+        </ul>
+      }
+      
     </div>
   )
 }
