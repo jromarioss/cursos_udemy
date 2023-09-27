33e28fbfa9b617ac05dd677133c1c9b04f093cb9
JromarioSs <jromario2014@gmail.com>
2023-09-27 20:08:05 -0300

    react js continuando modulo10

--- reactjsMateusB/modulo10/src/firebase/config.js
+++ reactjsMateusB/modulo10/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore} from "firebase/firebase"
+import { getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDAryy38_A0mADGCKS5wgVfvr5aRiQJzqY",
