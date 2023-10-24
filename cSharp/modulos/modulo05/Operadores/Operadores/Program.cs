41db0e74342fdbd88c18e30219d98acdba63f8f4
JromarioSs <jromario2014@gmail.com>
2023-10-24 20:17:42 -0300

    csharp modulo05

---
+++ cSharp/modulos/modulo05/Operadores/Operadores/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Aritméticos
+            //int num1 = 10;
+            //int num2 = 2;
+            //int resultado = num1 + num2;
+
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 % num2);
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Precedência
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
+
+            //double res = (num1 + num2) * (num3 / 2);
+
+            //Console.WriteLine(res);
+            //Console.ReadKey();
+            #endregion
+
+            #region Incremento e Decremento
+            //int num1 = 10;
+            //num1++; // 11
+            //num1--; // 09
+            //++num1; // 11
+            //--num1; // 09
+            #endregion
+
+            #region Concatenação
+            //string nome = "Romário ";
+            //string sobrenome = "Santos";
+            //string nomeCompleto = nome + sobrenome; // junta duas stringS
+            #endregion
+
+            #region Atribuição
+            //int num1 = 10;
+            //num1 = 20; // sobreecreve o valor
+            //num1 += 10; // 30
+            //num1 -= 2; // 28
+            //num1 *= 2; // 56
+            //num1 /= 2; // 26
+            //num1 %= 2; // 0
+            #endregion
+
+            #region Igualdade
+            //bool res = 10 == 10; // true
+            //string name1 = "Maria";
+            //string name2 = "Pedro";
+
+            //bool igual = name1 != name2; // true
+            #endregion
+
+            #region Relacionais
+            //bool res = 100 < 50; // false
+            //res = 100 > 50; // true
+            //res = 100 <= 50; // false
+            //res = 100 >= 50; // true
+            #endregion
+
+            #region Lógico
+            //bool res1 = 100 > 50; // true
+            //bool res2 = 50 == 50; // true
+
+            //bool final = res1 && res2; // se os 2 true então é true
+            //bool fina2 = res1 || res2; // pelo menos um true é true
+            #endregion
+        }
+    }
+}
