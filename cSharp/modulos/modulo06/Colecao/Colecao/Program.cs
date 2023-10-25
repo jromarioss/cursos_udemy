3cf9e429e599676fd59897c60fbe07d5e8020da2
JromarioSs <jromario2014@gmail.com>
2023-10-25 20:15:55 -0300

    csharp modulo06

---
+++ cSharp/modulos/modulo06/Colecao/Colecao/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Vetores
+            //string[] nomes =
+            //{
+            //    "Maria", "Pedro", "Miguel"
+            //};
+
+            //Console.WriteLine(nomes[2]); // Miguel
+            //Console.ReadKey();
+            #endregion
+
+            #region Vetores bidimensionais
+            int[,] numeros = new int[2, 3]; // 2 linhas e 3 colunas
+            numeros[0, 0] = 10;
+            numeros[0, 1] = 30;
+            numeros[0, 2] = 45; // [ 10, 30, 45 ]
+            numeros[1, 0] = 43; // [ 43, 03, 78 ]
+            numeros[1, 1] = 33;
+            numeros[1, 2] = 78;
+
+            Console.Write("[" + numeros[0, 0] + "]"); // 10
+            Console.Write("[" + numeros[0, 1] + "]"); // 30
+            Console.Write("[" + numeros[0, 2] + "]"); // 45
+            Console.WriteLine("");
+            Console.Write("[" + numeros[1, 0] + "]"); // 43
+            Console.Write("[" + numeros[1, 1] + "]"); // 33
+            Console.Write("[" + numeros[1, 2] + "]"); // 78
+            Console.WriteLine("");
+
+            string[,] nomes =
+            {
+                {"Pedro", "Julia", "Miguel"},
+                {"Giovana", "José", "Olavo"}
+            };
+
+            Console.Write("[" + nomes[0, 0] + "]");
+            Console.Write("[" + nomes[0, 1] + "]");
+            Console.Write("[" + nomes[0, 2] + "]");
+            Console.WriteLine();
+            Console.Write("[" + nomes[1, 0] + "]");
+            Console.Write("[" + nomes[1, 1] + "]");
+            Console.Write("[" + nomes[1, 2] + "]");
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ cSharp/modulos/modulo06/Colecao/MultiplicarMatrizes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicarMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = new int[2, 3];
+            int[,] matriz2 = new int[3, 2];
+            int[,] resultado = new int[2, 2];
+
+            Console.WriteLine("Preencher a matriz # 1");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("#1. Posição [" + i + "][" + j + "]: ");
+                    matriz1[i ,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nPreencher a matriz # 1");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("#1. Posição [" + i + "][" + j + "]: ");
+                    matriz2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nResultado da matriz # 1 X matriz # 2");
+            resultado[0, 0] = (matriz1[0, 0] * matriz2[0, 0]) + (matriz1[0,1] * matriz2[1,0]) + (matriz1[0, 2] * matriz2[2, 0]);
+            resultado[1, 0] = (matriz1[1, 0] * matriz2[0, 0]) + (matriz1[1,1] * matriz2[1,0]) + (matriz1[1, 2] * matriz2[2, 0]);
+            resultado[0, 1] = (matriz1[0, 0] * matriz2[0, 1]) + (matriz1[0,1] * matriz2[1,1]) + (matriz1[0, 2] * matriz2[2, 1]);
+            resultado[1, 1] = (matriz1[1, 0] * matriz2[0, 1]) + (matriz1[1,1] * matriz2[1,1]) + (matriz1[1, 2] * matriz2[2, 1]);
+
+            Console.WriteLine("[" + resultado[0, 0] + "][" + resultado[0,1] + "]");
+            Console.WriteLine("[" + resultado[1, 0] + "][" + resultado[1,1] + "]");
+            Console.ReadKey();
+        }
+    }
+}
