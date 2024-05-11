dd10ac28d39a54572147accf6a20f731109314a4
jromarioss <jromario2014@gmail.com>
2024-05-11 08:11:18 -0300

    4 Validação com DTO

---
+++ nestjs/nestjsHCODE/modulo04/src/user/DTO/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsString, IsStrongPassword } from "class-validator"
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6, // ter 6 caracteres
+    minUppercase: 1, // pelo menos ter um caractere maiúsculo
+  })
+  password: string;
+}
--- nestjs/nestjsHCODE/modulo04/src/user/user.controller.ts
+++ nestjs/nestjsHCODE/modulo04/src/user/user.controller.ts
@@ -1,70 +1,39 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { CreateUserDTO } from './dtos/create-user.dto';
-import { UpdatePatchUserDTO } from './dtos/update-patch-user.dto';
-import { UpdatePutUserDTO } from './dtos/update-put-user.dto copy';
 
-@Controller('users')
+import { Controller, Post, Body, Get, Param, Put, Patch, Delete, ParseIntPipe } from '@nestjs/common';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UpdatePutUserDTO } from './DTO/update-put-user.dto';
+import { UpdatePatchUserDTO } from './DTO/update-patch-user.dto';
+
+@Controller('users')// /users
 export class UserController {
-  @Post()
-  async create(@Body() body: CreateUserDTO) {
-    return { body };
+
+  @Post('postUser')
+  async postUser(@Body() body: CreateUserDTO) {
+    return {body};
   }
 
-  @Get()
-  async read() {
-    return {
-      users: [],
-    };
+  @Get('getAllUsers')
+  async getAllUsers() {
+    return {users: []}
   }
 
-  @Get(':id')
-  async readOne(@Param('id', ParseIntPipe) id: number) {
-    return {
-      user: {},
-      id,
-    };
+  @Get('getUser/:id')
+  async getUser(@Param('id', ParseIntPipe) id) {
+    return {user: {}, id}
   }
 
-  @Put(':id')
-  async update(
-    @Body() { name, email, password }: UpdatePutUserDTO,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
-    return {
-      method: 'put',
-      name,
-      email,
-      password,
-      id,
-    };
+  @Put('putUser/:id')
+  async putUser(@Body() { name, email, password }: UpdatePutUserDTO, @Param('id', ParseIntPipe) id) {
+    return { name, email, password, id }
   }
 
-  @Patch(':id')
-  async updatePartial(
-    @Body() { name }: UpdatePatchUserDTO,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
-    return {
-      method: 'patch',
-      name,
-      id,
-    };
+  @Patch('patchUser/:id')
+  async patchUser(@Body() body: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id) {
+    return { body, id }
   }
 
-  @Delete(':id') // converte para number
-  async delete(@Param('id', ParseIntPipe) id: number) {
-    return {
-      id,
-    };
+  @Delete('deleteUser/:id')
+  async deleteUser(@Param('id', ParseIntPipe) id) { // ParseIntPipe converte ele para um number
+    return { id }
   }
 }
