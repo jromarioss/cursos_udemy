7a9234ab35b186cf3ea95ca829b8b0123db48877
jromarioss <jromario2014@gmail.com>
2024-05-18 08:26:16 -0300

    modulo 06 novo

--- nestjs/nestjsHCODE/modulo06/src/auth/dto/auth-register.dto.ts
+++
@@ -1,3 +0,0 @@
-import { CreateUserDTO } from 'src/user/dtos/create-user.dto';
-
-export class AuthRegisterDTO extends CreateUserDTO {}
--- nestjs/nestjsHCODE/modulo06/src/user/dtos/update-patch-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDTO } from './create-user.dto';
-
-export class UpdatePatchUserDTO extends PartialType(CreateUserDTO) {}
